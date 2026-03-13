287a5995f2114d53f8e5e18d428f4a03accc2e8f
Juan Jose Soto Romero <juan.sotor@upb.edu.co>
2026-03-13 09:12:08 -0500

    agrego juego batalla RPG

---
+++ Videojuegos/Videojuegos/batallaRPG.cs
@@ -0,0 +1,24 @@
+﻿namespace Videojuegos
+{
+    public class batallaRPG
+    {
+        public string nombreJugador;
+        public int nivelJugador;
+        public int saludJugador;
+        public string nombreEnemigo;
+        public int nivelEnemigo;
+        public int saludEnemigo;
+
+
+        // constructor para inicializar los atributos
+        public batallaRPG(string nombreJugador, int nivelJugador, int saludJugador, string nombreEnemigo, int nivelEnemigo, int saludEnemigo)
+        {
+            this.nombreJugador = nombreJugador;
+            this.nivelJugador = nivelJugador;
+            this.saludJugador = saludJugador;
+            this.nombreEnemigo = nombreEnemigo;
+            this.nivelEnemigo = nivelEnemigo;
+            this.saludEnemigo = saludEnemigo;
+        }
+    }
+}
