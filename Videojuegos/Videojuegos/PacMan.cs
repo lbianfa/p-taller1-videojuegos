a032cc888f9ff0ad60903c3a16c2688b7bab8184
B09S303est <B09S303est@MEDAULAS.UPB>
2026-03-13 08:50:55 -0500

    Agregar clase PacMan

---
+++ Videojuegos/Videojuegos/PacMan.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Videojuegos
+{
+    public class PacMan
+    {
+
+        public int Puntos { get; set; }
+    }
+}
