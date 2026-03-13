063d5ad828286116e1213dce9e7ff2a90fcdd7b9
VALENTINA <vale0612>
2026-03-13 08:58:10 -0500

    se crea la clase Mario Kart y se ponen los atributos

---
+++ Videojuegos/Videojuegos/MarioKart.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Videojuegos
+{
+    public class MarioKart
+    {
+        // atributos
+        public string NombreJugador { get; set; }
+        public string Personaje { get; set; }
+        public string Vehiculo { get; set; }
+
+        // constructor
+
+        public MarioKart(string nombreJugador, string personaje, string vehiculo)
+        {
+            NombreJugador = nombreJugador;
+            Personaje = personaje;
+            Vehiculo = vehiculo;
+        }
+
+    
+    }
+}
